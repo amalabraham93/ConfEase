0c06d67412a3a3671c10211dcc2d075051bb8441
amalabraham93 <amalabraham93@gmail.com>
2023-07-20 21:45:39 +0530

    last commit of the day with auth user reasy

--- src/app/interceptors/users/auth-user.interceptor.ts
+++ src/app/interceptors/users/auth-user.interceptor.ts
@@ -1,12 +1,5 @@
 import { Injectable } from '@angular/core';
-import {
-  HttpRequest,
-  HttpHandler,
-  HttpEvent,
-  HttpInterceptor,
-  HttpErrorResponse,
-  HttpResponse
-} from '@angular/common/http';
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor, HttpErrorResponse, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 import { CookieService } from 'ngx-cookie-service';
@@ -18,19 +11,21 @@ export class AuthUserInterceptor implements HttpInterceptor {
   constructor(private _cookie: CookieService, private _spinner: SpinnerService) { }
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-  const token = localStorage.getItem('jwt-user') ;
-   
-  let authReq: HttpRequest<any>;
+    const token = localStorage.getItem('jwt-user');
 
-  if (token) {
-    authReq = request.clone({ setHeaders: { Authorization: token } ,withCredentials:true});
-  } else {
-    authReq = request.clone();
+    if (token) {
+      // Add "Bearer" prefix to the token
+      const modifiedHeaders = request.headers.set('Authorization', 'Bearer ' + token);
+      // Clone the request with the modified headers
+      const authReq = request.clone({ headers: modifiedHeaders, withCredentials: true });
+
+      this._spinner.requestStarted();
+      return this.handler(next, authReq);
+    } else {
+      this._spinner.requestStarted();
+      return this.handler(next, request);
+    }
   }
-
-  this._spinner.requestStarted();
-  return this.handler(next, authReq);
-}
 
   handler(next: HttpHandler, request: HttpRequest<any>) {
     return next.handle(request)
